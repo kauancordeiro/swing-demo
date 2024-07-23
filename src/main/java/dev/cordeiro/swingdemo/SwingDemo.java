78f0ee76b8e9deeaa69b2debb3a524d3cb71e177
Kauan <cvitorcordeiro@gmail.com>
2024-07-22 21:30:56 -0300

    first commit

---
+++ src/main/java/dev/cordeiro/swingdemo/SwingDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dev.cordeiro.swingdemo;
+
+/**
+ *
+ * @author Kauan
+ */
+public class SwingDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
