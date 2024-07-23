ab273a7f2b2e894d6ea6b342a94a66fa1812f701
Kauan <cvitorcordeiro@gmail.com>
2024-07-23 00:05:06 -0300

    timer and jprogressbar

--- src/main/java/dev/cordeiro/swingdemo/InitialScreen.java
+++ src/main/java/dev/cordeiro/swingdemo/InitialScreen.java
@@ -2,6 +2,8 @@ package dev.cordeiro.swingdemo;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class InitialScreen {
     public static void main(String[] args) {
@@ -9,21 +11,43 @@ public class InitialScreen {
     }
 }
 
-class Init extends JFrame{
-    public Init(){
-        JButton btnInit = new JButton("Init form");
+class Init extends JFrame implements ActionListener{
+
+    int i;
+    JProgressBar jProgressBar;
+    JButton btnInit;
+    Timer timer;
+
+    public Init() {
+         btnInit = new JButton("Init form");
+         jProgressBar = new JProgressBar(0, 20);
+         timer = new Timer(250, this);
+         int i = 0;
+
+
 
         btnInit.addActionListener(e -> {
-            new RadioButtonDemo();
-            dispose();
+            timer.start();
         });
 
 
         add(btnInit);
+        add(jProgressBar);
         setLayout(new FlowLayout());
         setVisible(true);
-        setSize(200,200);
+        setSize(200, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(i == 20){
+            new RadioButtonDemo();
+            dispose();
+        }
+        i++;
+        jProgressBar.setValue(i);
 
     }
 }
